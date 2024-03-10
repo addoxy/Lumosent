2db2c2d7c29fde77cd731178a1e4373587401737
addoxy <adityakumar13043@gmail.com>
2024-03-10 20:55:55 +0530

    chore:  add try and catch blocks in get-metrics server actions

--- src/app/actions/get-metrics.ts
+++ src/app/actions/get-metrics.ts
@@ -35,21 +35,26 @@ export const getPerfectDays = async () => {
     return null;
   }
 
-  const distinctDates = await prisma.habitEntry.groupBy({
-    by: ['completedAt'],
-    where: { habit: { userId: user.id } },
-    _count: { completedAt: true },
-    having: {
-      completedAt: {
-        _count: {
-          gte: await prisma.habit.count({ where: { userId: user.id } }),
+  try {
+    const distinctDates = await prisma.habitEntry.groupBy({
+      by: ['completedAt'],
+      where: { habit: { userId: user.id } },
+      _count: { completedAt: true },
+      having: {
+        completedAt: {
+          _count: {
+            gte: await prisma.habit.count({ where: { userId: user.id } }),
+          },
         },
       },
-    },
-  });
+    });
 
-  const completedDaysCount = getDaysCount(distinctDates);
-  return completedDaysCount;
+    const completedDaysCount = getDaysCount(distinctDates);
+    return completedDaysCount;
+  } catch (err) {
+    ServerResponse('Server error', 500);
+    return null;
+  }
 };
 
 export const getUserLongestStreak = async () => {
@@ -59,16 +64,21 @@ export const getUserLongestStreak = async () => {
     return null;
   }
 
-  const entriesByHabit = await prisma.habitEntry.groupBy({
-    by: ['habitId', 'completedAt'],
-    where: { habit: { userId: user.id } },
-    orderBy: { completedAt: 'desc' },
-    _max: { completedAt: true },
-  });
+  try {
+    const entriesByHabit = await prisma.habitEntry.groupBy({
+      by: ['habitId', 'completedAt'],
+      where: { habit: { userId: user.id } },
+      orderBy: { completedAt: 'desc' },
+      _max: { completedAt: true },
+    });
 
-  // Get the most recent completion date per habit
-  const longestStreak = await getLongestStreak(entriesByHabit);
-  return longestStreak;
+    // Get the most recent completion date per habit
+    const longestStreak = await getLongestStreak(entriesByHabit);
+    return longestStreak;
+  } catch (err) {
+    ServerResponse('Server error', 500);
+    return null;
+  }
 };
 
 export const getFavoriteHabit = async () => {
@@ -78,27 +88,32 @@ export const getFavoriteHabit = async () => {
     return null;
   }
 
-  const result = await prisma.habitEntry.groupBy({
-    by: ['habitId'],
-    where: {
-      habit: { userId: user.id },
-    },
-    _count: {
-      habitId: true,
-    },
-    orderBy: {
-      _count: { habitId: 'desc' },
-    },
-    take: 1, // Fetch only the top result
-  });
+  try {
+    const result = await prisma.habitEntry.groupBy({
+      by: ['habitId'],
+      where: {
+        habit: { userId: user.id },
+      },
+      _count: {
+        habitId: true,
+      },
+      orderBy: {
+        _count: { habitId: 'desc' },
+      },
+      take: 1, // Fetch only the top result
+    });
 
-  // Get the habit based on the habitId
-  const habitId = result[0].habitId;
-  const habit = await prisma.habit.findUnique({
-    where: { id: habitId },
-  });
+    // Get the habit based on the habitId
+    const habitId = result[0].habitId;
+    const habit = await prisma.habit.findUnique({
+      where: { id: habitId },
+    });
 
-  return habit;
+    return habit;
+  } catch (err) {
+    ServerResponse('Server error', 500);
+    return null;
+  }
 };
 
 export const getGraphStats = async (daysBack = 7) => {
@@ -110,19 +125,24 @@ export const getGraphStats = async (daysBack = 7) => {
 
   const startDate = sub(new Date(), { days: daysBack });
 
-  const dailyCounts = await prisma.habitEntry.groupBy({
-    by: ['completedAt'],
-    where: {
-      completedAt: { gte: startDate },
-      habit: { userId: user.id },
-    },
-    _count: { habitId: true },
-  });
+  try {
+    const dailyCounts = await prisma.habitEntry.groupBy({
+      by: ['completedAt'],
+      where: {
+        completedAt: { gte: startDate },
+        habit: { userId: user.id },
+      },
+      _count: { habitId: true },
+    });
 
-  const totalHabitCount = await prisma.habit.count({
-    where: { userId: user.id },
-  });
+    const totalHabitCount = await prisma.habit.count({
+      where: { userId: user.id },
+    });
 
-  const results = formatDayStats(dailyCounts, totalHabitCount);
-  return results;
+    const results = formatDayStats(dailyCounts, totalHabitCount);
+    return results;
+  } catch (err) {
+    ServerResponse('Server error', 500);
+    return null;
+  }
 };
