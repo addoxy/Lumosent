e284aabecad7563e67b694340fb95178333b650a
addoxy <adityakumar13043@gmail.com>
2024-03-11 16:24:30 +0530

    chore:  handle error state on tracker page

--- src/app/tracker/page.tsx
+++ src/app/tracker/page.tsx
@@ -12,6 +12,16 @@ const page = async () => {
       <div className="mb-5 mt-10 flex justify-end">
         <NewHabit />
       </div>
+      {!habits && (
+        <p className="mx-auto mt-20 text-lg text-zinc-300">
+          &#x1F641; Something went wrong! Please try again later!
+        </p>
+      )}
+      {habits?.length == 0 && (
+        <p className="mx-auto mt-20 text-lg text-zinc-300">
+          Create a new habit by clicking on the &apos;+ New Habit&apos; button
+        </p>
+      )}
       {habits && habits.length > 0 && <GridView habits={habits} />}
       {habits && habits.length > 0 && <TableView habits={habits} />}
       {habits && habits.length > 0 && <ColumnView habits={habits} />}
