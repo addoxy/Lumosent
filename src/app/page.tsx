13ad404e10061413ef80044f8266e6187b79f736
addoxy <adityakumar13043@gmail.com>
2024-03-13 15:28:01 +0530

    chore:  add navbar on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import { Logo } from '@/components/Icons';
+import { Button } from '@/components/vendor/button';
+import Link from 'next/link';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <main className="flex flex-col">
+      <nav className="flex items-center justify-between">
+        <Link href="/" className="flex items-center gap-x-3">
+          <Logo className="size-7 stroke-zinc-300 text-zinc-950 hover:text-zinc-900" />
+          <span className="text-xl font-semibold text-zinc-100">Lumosent</span>
+        </Link>
+        <Button className="bg-zinc-200 font-medium text-zinc-800 hover:bg-zinc-400">
+          Sign in
+        </Button>
+      </nav>
+    </main>
+  );
 }
